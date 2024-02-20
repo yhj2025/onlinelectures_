8f400627b70276d356942673f3fab86927eb1b6a
yhj2025 <148745771+yhj2025@users.noreply.github.com>
2024-02-20 16:52:44 +0900

    Delete back_nodejs/database.js

--- back_nodejs/database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root', 
-    password : '0000',
-    database : 'my_project',
-    port : 3306
-});
